fe612959a7855d5d411d5696d32b9d74b9296c64
r-hashiguchi <r-hashiguchi@CD26308L.arksystems.co.jp>
2026-06-02 17:17:28 +0900

    p0501まで

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
@@ -1,5 +1,45 @@
 package jp.co.sss.practice.p04.q02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class Practice0402Controller {
+	@RequestMapping(path = "/numguess/start", method = RequestMethod.GET)
+	public String p0402first(HttpSession session) {
+		Integer hitNumber = (int) (Math.floor(Math.random() * 9) + 1);
+		session.setAttribute("hitNumber", hitNumber);
+		System.out.println("当たりの数値: " + hitNumber);
+		return "practice04/02/numguess_start";
+	}
+
+	@GetMapping(path = "/numguess/input")
+	public String p0402second() {
+		return "practice04/02/numguess_input";
+	}
+
+	@GetMapping(path = "/numguess/judge")
+	public String p0402third(HttpSession session, Integer inputNum) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		if (hitNumber.equals(inputNum)) {
+			return "redirect:/numguess/hit";
+		} else {
+			return "practice04/02/numguess_judge";
+		}
+	}
+
+	@GetMapping(path = "/numguess/hit")
+	public String p0402forth(Model model, HttpSession session) {
+		Integer hitNumber = (Integer) session.getAttribute("hitNumber");
+		String message = "当たりです！ 正解は" + hitNumber + "でした。";
+		model.addAttribute("message", message);
+		session.removeAttribute("hitNumber");
+		return "practice04/02/numguess_end";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p05/entity/FruitsSeason.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/entity/FruitsSeason.java
@@ -1,5 +1,62 @@
 package jp.co.sss.practice.p05.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fruits_season")
 public class FruitsSeason {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_fruits_season")
+	@SequenceGenerator(name = "seq_fruits_season", sequenceName = "seq_fruits_season", allocationSize = 1)
+	private Integer fruitId;
+	private String fruitName;
+	private Integer seasonMonth;
+
+	/**
+	 * @return id
+	 */
+	public Integer getId() {
+		return fruitId;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(Integer id) {
+		this.fruitId = id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return fruitName;
+	}
+
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.fruitName = name;
+	}
+
+	/**
+	 * @return season
+	 */
+	public Integer getSeason() {
+		return seasonMonth;
+	}
+
+	/**
+	 * @param season セットする season
+	 */
+	public void setSeason(Integer season) {
+		this.seasonMonth = season;
+	}
 
 }
